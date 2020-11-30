013c3c45b9bfa518ed35284e627534f38f477e9a
royalvison <75214592+royalvison@users.noreply.github.com>
2020-11-30 01:15:41 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+import csv
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    saveInCSV = list()
+
+    output = round(prediction[0], 2)
+
+    saveInCSV = list(final_features[0])
+    saveInCSV.append(output)
+    myFile = open('result_data.csv', 'a')
+      
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows([saveInCSV])
+    myFile.close()
+
+    if bool(output):
+        return  render_template('index.html', prediction_text='Yes, as per given data person is having Heart disease')
+    else:
+        return  render_template('index.html', prediction_text='No, as per given data person does not having Heart disease')
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
